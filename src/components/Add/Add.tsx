7c6a90d5a3e43489913fcfc33a15aa9f61aca800
prashanth-sams <sams.prashanth@gmail.com>
2019-09-14 18:38:10 +0400

    add component

--- src/components/Add/Add.tsx
+++ src/components/Add/Add.tsx
@@ -1,28 +1,166 @@
-import * as React from 'react';
+import * as React from 'react'
+import KeywordsProps from '../../interfaces/KeywordsProps.interface'
+import Select, { components } from 'react-select';
+import axios from 'axios';
+import { colourOptions } from './data';
 
-interface AddProps {
-
+interface KeywordsState {
+  error: boolean
+  tag_name: string[],
+  book_name: string,
+  chapter_number: number,
+  verse_number: string,
+  verse_context: string
 }
 
-interface AddState {
-    name: string;
-}
+const MultiValueLabel = (props) => {
+	return (<components.MultiValueLabel {...props}/>);
+};
 
-class Add extends React.Component<AddProps, AddState> {
-    constructor(props: AddProps) {
-        super(props);
-        this.state = {name: 'Everton'};
-    }
+export class Add extends React.Component<KeywordsProps, KeywordsState> {
+  constructor(props: KeywordsProps) {
+    super(props);
+    this.onChangeTagName = this.onChangeTagName.bind(this);
+		this.onChangeBookName = this.onChangeBookName.bind(this);
+		this.onChangeChapterNumber = this.onChangeChapterNumber.bind(this);
+		this.onChangeVerseNumber = this.onChangeVerseNumber.bind(this);
+		this.onChangeVerseContext = this.onChangeVerseContext.bind(this);
+		this.onSubmit= this.onSubmit.bind(this);
 
-    public render() {
-        return (
-          <div className="hello">
-            <div className="greeting">
-              Add
-            </div>
+    this.state = {
+      error: false,
+			tag_name : [],
+			book_name : '',
+			chapter_number: null,
+			verse_number: '',
+			verse_context: ''
+    };
+    axios.defaults.baseURL = 'http://localhost:4000/keyword';
+  }
+
+  public onSubmit = () => {
+    console.log(`The values are ${this.state.tag_name}, ${this.state.book_name}, ${this.state.chapter_number}, ${this.state.verse_number}, and ${this.state.verse_context}`)
+    
+    const obj = {
+			tag_name : this.state.tag_name,
+			book_name : this.state.book_name,
+			chapter_number : this.state.chapter_number,
+			verse_number : this.state.verse_number,
+			verse_context : this.state.verse_context
+    };
+    
+    axios.post('/add', obj)
+			.then( res=> {
+        if(res.status !== 200) {
+          this.setState({ error: true});
+          return;
+        }
+				console.log(res.data)
+			});
+  }
+
+  public onChangeTagName = (newValue, actionMeta) => {
+    console.group('Value Changed');
+		console.log(newValue);
+		console.log(`action: ${actionMeta.action}`);
+		console.groupEnd();
+
+		const values = [];
+		for (let i = 0, l = newValue.length; i < l; i++) {
+			values.push(newValue[i].value);
+		}
+		
+		this.setState({
+			tag_name: values
+		});
+  }
+
+  public onChangeBookName = (e) => {
+		this.setState({
+			book_name : e.target.value
+		});
+  }
+  
+  public onChangeChapterNumber = (e) => {
+		this.setState({
+			chapter_number : e.target.value
+		});
+  }
+  
+  public onChangeVerseNumber = (e) => {
+		this.setState({
+			verse_number : e.target.value
+		});
+  }
+  
+  public onChangeVerseContext = (e) => {
+		this.setState({
+			verse_context : e.target.value
+		});
+	}
+
+  public render() {
+    // const {tag_name, book_name} = this.props;
+    return (
+      <div style={{marginTop: 10}}>
+				<h1>Add Verse</h1>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group" style={{ fontSize: '0.9rem' }}>
+            <Select
+              name="tags"
+              isMulti={true}
+              onChange={this.onChangeTagName}
+              options={colourOptions}
+              placeholder="Tags [eg., love]"
+              autoFocus={true}
+              components={{ MultiValueLabel }}
+              styles={{ multiValueLabel: (base) => ({ ...base, backgroundColor: colourOptions[2].color, color: 'white' }) }}
+              required={true}
+            />
           </div>
-        );
-    }
+          <div className="form-group">
+            <input type="text" className="form-control"
+              name="book"
+              placeholder="Book [eg., matthew]"
+              value={this.state.book_name}
+              onChange={this.onChangeBookName}
+              required={true}
+            />
+          </div>
+          <div className="form-group">
+            <input type="text" className="form-control"
+              name="chapter"
+              placeholder="Chapter"
+              value={this.state.chapter_number || ''}
+              onChange={this.onChangeChapterNumber}
+              required={true}
+            />
+          </div>
+          <div className="form-group">
+            <input type="text" className="form-control"
+              name="verse number"
+              placeholder="Verse number"
+              value={this.state.verse_number}
+              onChange={this.onChangeVerseNumber}
+              required={true}
+            />
+          </div>
+          <div className="form-group">
+            <input type="text" className="form-control"
+              name="verse"
+              placeholder="Verse context"
+              value={this.state.verse_context}
+              onChange={this.onChangeVerseContext}
+              required={true}
+            />
+          </div>
+          <div className="form-group">
+            <input type="submit" value="Add Verse" className="btn btn-primary"/>
+          </div>
+        </form>
+      </div>
+    )
+  }
 }
 
-export default Add;
+export default Add
