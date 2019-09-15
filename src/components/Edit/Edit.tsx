1a69a3eb0b687a0d6e23d703466b80f966d1b90e
prashanth-sams <sams.prashanth@gmail.com>
2019-09-15 11:59:43 +0400

    added edit component

--- src/components/Edit/Edit.tsx
+++ src/components/Edit/Edit.tsx
@@ -1,28 +1,155 @@
-import * as React from 'react';
+import * as React from 'react'
+import EditProps from '../../interfaces/EditProps.interface'
+import axios from 'axios';
+import './_style.css';
+import { RouteComponentProps } from 'react-router';
 
-interface EditProps {
-
+declare module 'react' {
+  interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
+    // extends React's HTMLAttributes
+    align?: string;
+  }
 }
 
 interface EditState {
-    name: string;
+  error: boolean
+  tag_name: string[],
+  book_name: string,
+  chapter_number: number,
+  verse_number: string,
+  verse_context: string
 }
 
-class Edit extends React.Component<EditProps, EditState> {
-    constructor(props: EditProps) {
-        super(props);
-        this.state = {name: 'Everton'};
-    }
+export class Edit extends React.Component<EditProps & RouteComponentProps, EditState> {
+  constructor(props: EditProps & RouteComponentProps) {
+    super(props);
 
-    public render() {
-        return (
-          <div className="hello">
-            <div className="greeting">
-              Edit
-            </div>
+    this.state = {
+      error: false,
+			tag_name : [],
+			book_name : '',
+			chapter_number: null,
+			verse_number: '',
+			verse_context: ''
+    };
+    axios.defaults.baseURL = 'http://localhost:4000/keyword';
+  }
+
+  public componentDidMount = () => {
+    axios.get('/edit/'+(this.props.match.params as any).id)
+        .then(response => {
+            this.setState({ 
+              tag_name: response.data.tag_name, 
+              book_name: response.data.book_name,
+              chapter_number: response.data.chapter_number,
+              verse_number: response.data.verse_number,
+              verse_context: response.data.verse_context 
+            });
+        })
+        .catch(error => {
+            console.log(error);
+        })
+  }
+
+
+  public onChangeTagName = (e) => {
+    this.setState({
+      tag_name: e.target.value
+    });
+  }
+
+  public onChangeBookName = (e) => {
+      this.setState({
+        book_name: e.target.value
+      })  
+  }
+  
+  public onChangeChapterNumber = (e) => {
+      this.setState({
+        chapter_number: e.target.value
+      })
+  }
+
+  public onChangeVerseNumber = (e) => {
+    this.setState({
+      verse_number: e.target.value
+    })
+  }
+
+  public onChangeVerseContext = (e) => {
+    this.setState({
+      verse_context: e.target.value
+    })
+  }
+
+  public onSubmit = (e) => {
+    e.preventDefault();
+    const obj = {
+      tag_name: this.state.tag_name,
+      book_name: this.state.book_name,
+      chapter_number: this.state.chapter_number,
+      verse_number: this.state.verse_number,
+      verse_context: this.state.verse_context
+    };
+    axios.post('/update/'+(this.props.match.params as any).id, obj)
+        .then(res => console.log(res.data));
+
+    this.props.history.push('/index');
+  }
+
+
+  public render() {
+    return (
+      <div style={{ marginTop: 10 }}>
+        <h3 align="center">Update Verse</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Tags: </label>
+            <input type="text" className="form-control" 
+              name="tags" 
+              value={this.state.tag_name} 
+              onChange={this.onChangeTagName} 
+            />
           </div>
-        );
-    }
+          <div className="form-group">
+            <label>Book: </label>
+            <input type="text" className="form-control" 
+              name="book" 
+              value={this.state.book_name} 
+              onChange={this.onChangeBookName} 
+            />
+          </div>
+          <div className="form-group">
+            <label>Chapter: </label>
+            <input type="text" className="form-control" 
+              name="chapter" 
+              value={this.state.chapter_number} 
+              onChange={this.onChangeChapterNumber} 
+            />
+          </div>
+          <div className="form-group">
+            <label>Verse number: </label>
+            <input type="text" className="form-control" 
+              name="verse number" 
+              value={this.state.verse_number} 
+              onChange={this.onChangeVerseNumber} 
+            />
+          </div>
+          <div className="form-group">
+            <label>Verse: </label>
+            <input type="text" className="form-control" 
+              name="verse" 
+              value={this.state.verse_context} 
+              onChange={this.onChangeVerseContext} 
+            />
+          </div>
+          <div className="form-group">
+            <input type="submit" value="Update Verse" className="btn btn-primary" />
+          </div>
+        </form>
+      </div>
+    )
+  }
 }
 
-export default Edit;
+export default Edit
