8af7888568e14042bee15ada3aab2028aa4ea5c0
prashanth-sams <sams.prashanth@gmail.com>
2019-10-08 19:11:31 +0400

    added auto scroll

--- src/components/AddQuestion/AddBibleSection.tsx
+++ src/components/AddQuestion/AddBibleSection.tsx
@@ -1,10 +1,43 @@
 import * as React from 'react';
+import AddBibleSectionProps from '../../interfaces/AddBibleSectionProps.interface';
 
-export class AddBibleSection extends React.Component {
-    constructor(props){
+export class AddBibleSection extends React.Component<any, AddBibleSectionProps> {
+    constructor(props: AddBibleSectionProps){
         super(props);
+        console.log(props)
+
+        this.state = {
+            book_name : '',
+            chapter_number: null,
+            verse_number: '',
+            verse_context: ''
+        };
     }
 
+    public onChangeBookName = (e) => {
+		this.setState({
+			book_name : e.target.value
+		});
+    }
+    
+    public onChangeChapterNumber = (e) => {
+        this.setState({
+            chapter_number : e.target.value
+        });
+    }
+    
+    public onChangeVerseNumber = (e) => {
+        this.setState({
+            verse_number : e.target.value
+        });
+    }
+    
+    public onChangeVerseContext = (e) => {
+        this.setState({
+            verse_context : e.target.value
+        });
+    }
+    
     public render() {
         return (
             <div className="bible-container">
@@ -12,6 +45,8 @@ export class AddBibleSection extends React.Component {
                     <input type="text" className="form-control"
                         name="book"
                         placeholder="Book [eg., matthew]"
+                        value={this.state.book_name}
+                        onChange={this.onChangeBookName}
                         required={true}
                     />
                 </div>
@@ -19,6 +54,8 @@ export class AddBibleSection extends React.Component {
                     <input type="text" className="form-control"
                         name="chapter"
                         placeholder="Chapter"
+                        value={this.state.chapter_number || ''}
+                        onChange={this.onChangeChapterNumber}
                         required={true}
                     />
                 </div>
@@ -26,6 +63,8 @@ export class AddBibleSection extends React.Component {
                     <input type="text" className="form-control"
                         name="verse number"
                         placeholder="Verse number"
+                        value={this.state.verse_number}
+                        onChange={this.onChangeVerseNumber}
                         required={true}
                 />
                 </div>
@@ -33,6 +72,8 @@ export class AddBibleSection extends React.Component {
                     <textarea id="form7" className="md-textarea form-control" rows={4}
                         name="verse"
                         placeholder="Verse context"
+                        value={this.state.verse_context}
+                        onChange={this.onChangeVerseContext}
                     />
                 </div>
             </div>
